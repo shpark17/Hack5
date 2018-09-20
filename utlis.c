ae59a232ebfe443156b84f277b412b35ab30cbfc
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-20 11:36:48 -0500

    Update utlis.c

--- utlis.c
+++ utlis.c
@@ -5,6 +5,10 @@ double degreesToRadians(double degree) {
   return (degree/180)*M_PI;
 }
 double getAirDistance(double originLatitude, double originLongitude, destinationLatitude, destinationLongitude) {
+  degreesToRadians(originLatitude);
+  degreesToRadians(originLongitude);
+  degreesToRadians(destinationLatitude);
+  degreesToRadians(destinationLongitude);
   return acos(sin(originLatitude)*sin(destinationLatitude)+cos(originLatitude)*cos(destinationLatitude)*cos(destinationLongitude-originLongitude) * 6371;
 }
 double lorentzTimeDilation(double t, double percentC) {
