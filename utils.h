8b2edbed8d60990a4d1f297d7b4c2becf45cfdd5
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-20 11:22:27 -0500

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,8 +1,6 @@
 double degreesToRadians(double degree) {
-  return (degree/180)*M_PI
 }
 double getAirDistance(double originLatitude, double originLongitude, destinationLatitude, destinationLongitude) {
-  return acos(sin(originLatitude)*sin(destinationLatitude)+cos(originLatitude)*cos(destinationLatitude)*cos(destinationLongitude-originLongitude) * 6371
 }
 double loretzTimeDilation(double t, double percentC) {
 }
